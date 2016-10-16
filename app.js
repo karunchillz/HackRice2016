d41d363dee338151b41b863cf3eb48d7fd408c49
Karunanidhi Devanidhi <karunchillz@Karunanidhis-MacBook-Air.local>
2016-10-15 21:23:43 -0500

    Changes

--- app.js
+++ app.js
@@ -1,38 +1,167 @@
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-var fs = require('fs');
-
-var visual_recognition = new VisualRecognitionV3({
-  api_key: '',
-  version_date: '2016-05-19'
-});
-
-var params_couple = {
-  images_file: fs.createReadStream('./resources/images/couple.jpg')
-};
-
-var params_girls = {
-  images_file: fs.createReadStream('./resources/images/girls.jpg')
-};
-
-var params_family = {
-  images_file: fs.createReadStream('./resources/images/family.jpg')
-};
-
-var params_dadAndGirl = {
-  images_file: fs.createReadStream('./resources/images/dadAndGirl.jpg')
-};
-
-/*visual_recognition.detectFaces(params_couple, function (err, keywords) {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log(JSON.stringify(keywords, null, 2));
-});*/
-
 var fs = require("fs"),
     http = require("http"),
     url = require("url"),
     path = require("path");
+
+var visual_recognition = new VisualRecognitionV3({
+  api_key: '37745a59c469e745974eac39b50a52752a803887',
+  version_date: '2016-05-19'
+});
+
+var matchedEntity = '';
+
+var params_test = {
+  url: 'http://13.84.145.193:9000/static/img.jpg'//fs.createReadStream('./resources/images/test_3.jpg')
+};
+
+var count = 5;
+var magicRatio = 1;
+
+var tempInterval = setInterval(function(){
+  if(count < 1)
+    clearInterval(tempInterval);
+  else
+    count = count - 1;
+
+  getImageFromNest();
+
+},10000);
+
+function recognizeFaces(parameters, recognizeTextResult){
+  console.log('recognizeTextResult',recognizeTextResult);
+  console.log('parameters',parameters);
+  var maleAgeMapping = initializeAgeMapping();
+  var femaleAgeMapping = initializeAgeMapping();
+  var numberOfImages = 1;
+  var maleNumber = 0;
+  var femaleNumber = 0;
+
+  visual_recognition.detectFaces(parameters, function (err, keywords) {
+    if (err)
+      console.log('error:', err);
+    else{
+      console.log(JSON.stringify(keywords, null, 2));
+      if(keywords.images_processed == numberOfImages){
+        var faces = keywords.images[0].faces;
+        faces.forEach(function(item,index){
+          var gender = item.gender.gender;
+          var avgAge = (item.age.max + item.age.min)/2;
+          if(gender == 'FEMALE'){
+            femaleNumber = femaleNumber + 1;
+            if(avgAge <= 20){
+              var tempIndex = femaleAgeMapping.indexOf('<=20');
+              femaleAgeMapping[tempIndex] = femaleAgeMapping[tempIndex] + 1;
+            }else if(21<= avgAge <= 40){
+              var tempIndex = femaleAgeMapping.indexOf('21-40');
+              femaleAgeMapping[tempIndex] = femaleAgeMapping[tempIndex] + 1;
+            }else if(41<= avgAge <= 60){
+              var tempIndex = femaleAgeMapping.indexOf('41-60');
+              femaleAgeMapping[tempIndex] = femaleAgeMapping[tempIndex] + 1;
+            }else if(avgAge >= 60){
+              var tempIndex = femaleAgeMapping.indexOf('>=61');
+              femaleAgeMapping[tempIndex] = femaleAgeMapping[tempIndex] + 1;
+            }
+          }else{
+            maleNumber = maleNumber + 1;
+            if(avgAge <= 20){
+              var tempIndex = maleAgeMapping.indexOf('<=20');
+              maleAgeMapping[tempIndex] = maleAgeMapping[tempIndex] + 1;
+            }else if(21<= avgAge <= 40){
+              var tempIndex = maleAgeMapping.indexOf('21-40');
+              maleAgeMapping[tempIndex] = maleAgeMapping[tempIndex] + 1;
+            }else if(41<= avgAge <= 60){
+              var tempIndex = maleAgeMapping.indexOf('41-60');
+              maleAgeMapping[tempIndex] = maleAgeMapping[tempIndex] + 1;
+            }else if(avgAge >= 60){
+              var tempIndex = maleAgeMapping.indexOf('>=61');
+              maleAgeMapping[tempIndex] = maleAgeMapping[tempIndex] + 1;
+            }
+          }
+          if(faces.length == (index+1)){
+            magicRatio = maleNumber / femaleNumber;
+            if(magicRatio < 0.6)
+              matchedEntity = 'Apple.mp4';
+            else if(0.60 <= magicRatio <= 1.4)
+              matchedEntity = 'Chevron.mp4';
+            else if(magicRatio > 1.4)
+              matchedEntity = 'CapitalOne.mp4';
+          } 
+        });
+      }
+    }
+  });
+
+}
+
+
+function initializeAgeMapping(){
+  var genderAgeMapping = [];
+  genderAgeMapping.push('<=20',0);
+  genderAgeMapping.push('21-40',0);
+  genderAgeMapping.push('41-60',0);
+  genderAgeMapping.push('>=61',0);
+  return genderAgeMapping;
+}
+
+function recognizeText(parameters){
+  console.log('recognizeText',parameters);
+  var sponsors = ['apple', 'capital', 'one', 'chevron', 'facebook', 'google', 'hewlett', 'packard', 'ibm', 'indeed', 'mlh'];
+  var sponsorMessage = ['Apple', 'CapitalOne', 'CapitalOne', 'Chevron', 'Facebook', 'Google', 'HP', 'HP', 'IBM', 'Indeed','MLH'];
+  var numberOfImages = 1;
+  var matchingCompany = '';
+  visual_recognition.recognizeText(parameters, function (err, keywords) {
+    if (err)
+      console.log('error:', err);
+    else{
+      console.log(JSON.stringify(keywords, null, 2));
+      console.log('keywords.images_processed',keywords.images_processed);
+      if(keywords.images_processed == numberOfImages){
+        var imageTextConsolidated = keywords.images[0].text;
+        sponsors.forEach(function(item,index){
+          console.log('item',item);
+          if(!matchingCompany && imageTextConsolidated.indexOf(item) > -1){
+            matchingCompany = sponsorMessage[index];
+          }
+          if((sponsors.length) == (index+1)){
+            console.log('matchingCompany',matchingCompany);
+            recognizeFaces(parameters,matchingCompany);
+          }
+        });
+
+      }
+    }
+  });
+
+}
+
+function getImageFromNest(){
+
+  http.get("http://13.84.145.193:9000", function(res) {
+    var body = ''; // Will contain the final response
+    // Received data is a buffer.
+    // Adding it to our body
+    res.on('data', function(data){
+      body += data;
+    });
+    // After the response is completed, parse it and log it to the console
+    res.on('end', function() {
+      var parsed = JSON.parse(body);
+      console.log(parsed);
+      var imageUrl = parsed.imageurl;
+      var parameters = {
+        url: imageUrl
+      };
+      recognizeText(parameters);
+    });
+  })
+  // If any error has occured, log error to console
+  .on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+
+  //recognizeText(params_test);
+}
 
 http.createServer(function (req, res) {
   console.log('req.url',req.url);
@@ -103,7 +232,7 @@ http.createServer(function (req, res) {
         });
     });
 
-  }else {
+  }else if(isImageRequest){
 
    if(req.url == '/resources/posters/Facebook.jpg'){
       imageName = 'Facebook.jpg';
@@ -131,6 +260,10 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
 
+  }else{
+
+    if(req.url == '/getMatchedEntity')
+      res.end(matchedEntity);
   }
 
 }).listen(8888);
