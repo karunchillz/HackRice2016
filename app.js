0ca100e6180005219511485f2d1b9fbcadfee340
Karunanidhi Devanidhi <karunchillz@Karunanidhis-MacBook-Air.local>
2016-10-16 00:58:41 -0500

    Changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var params_test = {
   url: 'http://13.84.145.193:9000/static/img.jpg'//fs.createReadStream('./resources/images/test_3.jpg')
 };
 
-var count = 5;
+var count = 50;
 var magicRatio = 1;
 
 var tempInterval = setInterval(function(){
@@ -27,7 +27,124 @@ var tempInterval = setInterval(function(){
 
   getImageFromNest();
 
-},10000);
+},15000);
+
+http.createServer(function (req, res) {
+  console.log('req.url',req.url);
+  var isVideoRequest = req.url.indexOf('.mp4') > -1 ? true : false;
+  var isImageRequest = req.url.indexOf('.jpg') > -1 ? true : false;
+
+  var videoName = 'IBM.mp4';
+  var imageName = 'IBM.jpg';
+
+  if(isVideoRequest){
+
+    if(req.url == '/Facebook.mp4'){
+      videoName = 'Facebook.mp4';
+    }else if(req.url == '/Google.mp4'){
+      videoName = 'Google.mp4';
+    }else if(req.url == '/Apple.mp4'){
+      videoName = 'Apple.mp4';
+    }else if(req.url == '/HP.mp4'){
+      videoName = 'HP.mp4';
+    }else if(req.url == '/Chevron.mp4'){
+      videoName = 'Chevron.mp4';
+    }else if(req.url == '/Indeed.mp4'){
+      videoName = 'Indeed.mp4';
+    }else if(req.url == '/CapitalOne.mp4'){
+      videoName = 'CapitalOne.mp4';
+    }else if(req.url == '/IBM.mp4'){
+      videoName = 'IBM.mp4';
+    }else{
+      videoName = 'MLH.mp4';
+    }
+
+    console.log('videoName',videoName);
+ 
+    var __dirname = 'resources/videos';
+    var file = path.resolve(__dirname,videoName);
+
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.writeHead(404, { "Content-Type": "text/html" });
+        }
+      res.end(err);
+      }
+      var range = req.headers.range;
+      if (!range) {
+       // 416 Wrong range
+       return res.writeHead(416, { "Content-Type": "text/html" });
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+
+  }else if(isImageRequest){
+
+   if(req.url == '/resources/posters/Facebook.jpg'){
+      imageName = 'Facebook.jpg';
+    }else if(req.url == '/resources/posters/Google.jpg'){
+      imageName = 'Google.jpg';
+    }else if(req.url == '/resources/posters/Apple.jpg'){
+      imageName = 'Apple.jpg';
+    }else if(req.url == '/resources/posters/HP.jpg'){
+      imageName = 'HP.jpg';
+    }else if(req.url == '/resources/posters/Chevron.jpg'){
+      imageName = 'Chevron.jpg';
+    }else if(req.url == '/resources/posters/Indeed.jpg'){
+      imageName = 'Indeed.jpg';
+    }else if(req.url == '/resources/posters/CapitalOne.jpg'){
+      imageName = 'CapitalOne.jpg';
+    }else if(req.url == '/resources/posters/IBM.jpg'){
+      imageName = 'IBM.jpg';
+    }else{
+      imageName = 'MLH.jpg';
+    }   
+    console.log('imageName',imageName); 
+    var __dirname = 'resources/posters';
+    var file = path.resolve(__dirname,imageName);
+    var img = fs.readFileSync(file);
+    res.writeHead(200, {'Content-Type': 'image/jpg' });
+    res.end(img, 'binary');
+
+  }else{
+
+    if(req.url == '/getMatchedEntity'){
+      console.log('matchedEntity_2',matchedEntity_2);
+      console.log('matchedEntity_1',matchedEntity_1);
+
+      var responseBody = {
+        matchedEntity_2: matchedEntity_2,
+        matchedEntity_1: matchedEntity_1
+      };
+      matchedEntity_2 = '';
+      matchedEntity_1 = '';
+      res.writeHead(200, {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "X-Requested-With"});
+      res.write(JSON.stringify(responseBody));
+      res.end();      
+    }
+  }
+
+}).listen(process.env.PORT||8888);
 
 function recognizeFaces(parameters, recognizeTextResult){
   console.log('recognizeTextResult',recognizeTextResult);
@@ -164,108 +281,3 @@ function getImageFromNest(){
 
   //recognizeText(params_test);
 }
-
-http.createServer(function (req, res) {
-  console.log('req.url',req.url);
-  var isVideoRequest = req.url.indexOf('.mp4') > -1 ? true : false;
-  var isImageRequest = req.url.indexOf('.jpg') > -1 ? true : false;
-
-  var videoName = 'IBM.mp4';
-  var imageName = 'IBM.jpg';
-
-  if(isVideoRequest){
-
-    if(req.url == '/Facebook.mp4'){
-      videoName = 'Facebook.mp4';
-    }else if(req.url == '/Google.mp4'){
-      videoName = 'Google.mp4';
-    }else if(req.url == '/Apple.mp4'){
-      videoName = 'Apple.mp4';
-    }else if(req.url == '/HP.mp4'){
-      videoName = 'HP.mp4';
-    }else if(req.url == '/Chevron.mp4'){
-      videoName = 'Chevron.mp4';
-    }else if(req.url == '/Indeed.mp4'){
-      videoName = 'Indeed.mp4';
-    }else if(req.url == '/CapitalOne.mp4'){
-      videoName = 'CapitalOne.mp4';
-    }else if(req.url == '/IBM.mp4'){
-      videoName = 'IBM.mp4';
-    }else{
-      videoName = 'MLH.mp4';
-    }
-
-    console.log('videoName',videoName);
- 
-    var __dirname = 'resources/videos';
-    var file = path.resolve(__dirname,videoName);
-
-    fs.stat(file, function(err, stats) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // 404 Error if file not found
-          return res.writeHead(404, { "Content-Type": "text/html" });
-        }
-      res.end(err);
-      }
-      var range = req.headers.range;
-      if (!range) {
-       // 416 Wrong range
-       return res.writeHead(416, { "Content-Type": "text/html" });
-      }
-      var positions = range.replace(/bytes=/, "").split("-");
-      var start = parseInt(positions[0], 10);
-      var total = stats.size;
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      var chunksize = (end - start) + 1;
-
-      res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
-      });
-
-      var stream = fs.createReadStream(file, { start: start, end: end })
-        .on("open", function() {
-          stream.pipe(res);
-        }).on("error", function(err) {
-          res.end(err);
-        });
-    });
-
-  }else if(isImageRequest){
-
-   if(req.url == '/resources/posters/Facebook.jpg'){
-      imageName = 'Facebook.jpg';
-    }else if(req.url == '/resources/posters/Google.jpg'){
-      imageName = 'Google.jpg';
-    }else if(req.url == '/resources/posters/Apple.jpg'){
-      imageName = 'Apple.jpg';
-    }else if(req.url == '/resources/posters/HP.jpg'){
-      imageName = 'HP.jpg';
-    }else if(req.url == '/resources/posters/Chevron.jpg'){
-      imageName = 'Chevron.jpg';
-    }else if(req.url == '/resources/posters/Indeed.jpg'){
-      imageName = 'Indeed.jpg';
-    }else if(req.url == '/resources/posters/CapitalOne.jpg'){
-      imageName = 'CapitalOne.jpg';
-    }else if(req.url == '/resources/posters/IBM.jpg'){
-      imageName = 'IBM.jpg';
-    }else{
-      imageName = 'MLH.jpg';
-    }   
-    console.log('imageName',imageName); 
-    var __dirname = 'resources/posters';
-    var file = path.resolve(__dirname,imageName);
-    var img = fs.readFileSync(file);
-    res.writeHead(200, {'Content-Type': 'image/jpg' });
-    res.end(img, 'binary');
-
-  }else{
-
-    if(req.url == '/getMatchedEntity')
-      res.end(matchedEntity_2+'-'+matchedEntity_1);
-  }
-
-}).listen(8888);
